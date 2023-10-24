8b72ca5ff53dfdee64e51c97b6d56d2d1bed54c2
Ansel Chang <ansel.chang@yahoo.com>
2023-10-24 14:48:27 -0400

    improve npm commands

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
+`npm start` - Run full application in production mode.
+
+`npm run build:client` - Build the client.
+
+`npm run build:server` - Build the server.
+
 `npm run clean` - will clean out the directories where the compiled output goes in case we need to make a fresh start.
 
 `npm run cp:www` - This copies ./server/bin/www to its proper location.
 
-`npm run dev` - Using Concurrently, we compile Typescript files every time there are changes, run the files in Node and watch for changes with Nodemon, then watch for changes to the Angular files and build those accordingly.
+`npm run dev` - Run in dev-friendly mode with HMR.
--- client/src/app/components/layout/play-page/play-page.component.ts
+++ client/src/app/components/layout/play-page/play-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import BoardState from '../../tetris/interactive-tetris-board/board-state';
+import GameStatus from 'client/src/app/models/immutable-tetris-models/game-status';
+import BinaryGrid, { BlockType } from 'client/src/app/models/mutable-tetris-models/binary-grid';
+import { TetrominoType } from 'client/src/app/models/immutable-tetris-models/tetromino';
 
 @Component({
   selector: 'app-play-page',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./play-page.component.scss']
 })
 export class PlayPageComponent {
+  
+  public boardState: BoardState;
 
+  constructor() {
+
+    const status = new GameStatus(18, 120, 400000);
+    const grid = new BinaryGrid();
+    // randomly populate grid
+    for (let x = 1; x <= 10; x++) {
+      for (let y = 1; y <= 20; y++) {
+        grid.setAt(x, y, Math.random() > 0.5 ? BlockType.FILLED : BlockType.EMPTY);
+      }
+    }
+
+    this.boardState = new BoardState(status, grid, TetrominoType.L_TYPE, TetrominoType.T_TYPE);
+  }
 }
