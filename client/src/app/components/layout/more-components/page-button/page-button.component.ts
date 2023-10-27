0d777d2bf865b570b186d3a3780d7bd0e5e92f39
Ansel Chang <ansel.chang@yahoo.com>
2023-10-26 22:07:12 -0400

    bugfix

--- client/src/app/components/layout/more-components/page-button/page-button.component.ts
+++ client/src/app/components/layout/more-components/page-button/page-button.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-button',
   templateUrl: './page-button.component.html',
   styleUrls: ['./page-button.component.scss']
 })
-export class PageButtonComponent {
+export class PageButtonComponent implements OnInit {
   @Input() icon!: string;
   @Input() title!: string;
   @Input() subtitle: string = "";
   @Input() routerLink!: string;
+
+  ngOnInit(): void {
+      //console.log("component", this.icon);
+  }
 
 }
