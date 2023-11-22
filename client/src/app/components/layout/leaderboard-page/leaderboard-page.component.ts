feb0dbc134ee5d8acd9c2211e0014f72f8d96aa1
Ansel Chang <ansel.chang@yahoo.com>
2023-11-22 09:50:52 -0500

    leaderboard tabs

--- client/src/app/components/layout/leaderboard-page/leaderboard-page.component.ts
+++ client/src/app/components/layout/leaderboard-page/leaderboard-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LeaderboardType } from 'server/database/leaderboard/leaderboard-schema';
 
 @Component({
   selector: 'app-leaderboard-page',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./leaderboard-page.component.scss']
 })
 export class LeaderboardPageComponent {
+
+  private type: LeaderboardType = LeaderboardType.OVERALL;
+
+  public isTypeOverall(): boolean {
+    return this.type === LeaderboardType.OVERALL;
+  }
+
+  public isType29(): boolean {
+    return this.type === LeaderboardType.START_29;
+  }
+
+  public setTypeOverall() {
+    this.type = LeaderboardType.OVERALL;
+  }
+
+  public setType29() {
+    this.type = LeaderboardType.START_29;
+  }
 
 }
